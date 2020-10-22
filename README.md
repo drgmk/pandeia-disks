a886c99b454c59d19d6a276d0b27047493a2c7c8
drgmk <grantkennedy@gmail.com>
2020-10-22 15:16:35 +0100

    updating for 2020

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # pandeia-disks
 JWST ETC simulations for disks
 
-For install instructions, see https://github.com/spacetelescope/pandeia-coronagraphy, noting that pandeia 1.1 (not pandeia 1.2) is needed.
+Quick example:
+```
+import jwst_pancake.scene as pc_scene
+import pandisk as pd
+targ = [pd.scene_star('a5v', 4.8)]
+targ = pd.add_ring(targ, 20, 5, 1, 180, 15, 45, 30)
+pc_scene.rotate_scene(targ, -30)
+pd.plot_disk_scene(targ)
+```
+
+For install instructions, see https://github.com/spacetelescope/pandeia-coronagraphy
--- pandisk/pandeia_funcs.py
+++ pandisk/pandeia_funcs.py
@@ -4,7 +4,42 @@ from matplotlib.patches import Ellipse
 import numpy as np
 import copy
 
-from dd.spectrum import bnu_wav_micron as bnu
+def bnu(wav_um,temp):
+    """Return a Planck function, avoiding overflows.
+        
+        Parameters
+        ----------
+        wave_um : ndarray of float
+        Wavelengths at which to compute flux.
+        temp : float
+        Temperature of blackbody.
+        """
+    k1 = 3.9728949e19
+    k2 = 14387.69
+    fact1 = k1/(wav_um**3)
+    fact2 = k2/(wav_um*temp)
+    if isinstance(wav_um,np.ndarray):
+        ofl = fact2 < 709
+        bnu = np.zeros(len(fact2)) + cfg.tiny
+        if np.any(ofl) == False:
+            return bnu
+        else:
+            bnu[ofl] = fact1[ofl]/(np.exp(fact2[ofl])-1.0)
+            return bnu
+    elif isinstance(temp,np.ndarray):
+        ofl = fact2 < 709
+        bnu = np.zeros(len(fact2)) + cfg.tiny
+        if np.any(ofl) == False:
+            return bnu
+        else:
+            bnu[ofl] = fact1/(np.exp(fact2[ofl])-1.0)
+            return bnu
+    else:
+        if fact2 > 709:
+            return cfg.tiny
+        else:
+            return fact1/(np.exp(fact2)-1.0)
+
 
 def scene_star(sptype, mag, id=1, band='johnson,v',
                name='generic source'):
